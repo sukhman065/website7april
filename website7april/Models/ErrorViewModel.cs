c62bf30891d8c46d4318095eeeb56ba8783a5e9f
Sukhmanpreet Kaur <2237483.cse.cec@cgc.edu.in>
2026-04-07 12:51:54 +0530

    Add project files.

---
+++ website7april/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace website7april.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
